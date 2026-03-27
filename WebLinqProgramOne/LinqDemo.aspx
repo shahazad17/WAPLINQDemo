2b635644a516dcfef7d6802fc3cc955cc677ec55
Shahazad M. Shaikh <shahazads@met.edu>
2026-03-27 10:35:02 +0530

    Added: LINQ to Object Mapping functionality added

---
+++ WebLinqProgramOne/LinqDemo.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LinqDemo.aspx.cs" Inherits="WebLinqProgramOne.LinqDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>LINQ Basics Demo</h2>
+
+            <asp:Label ID="lblCourse" runat="server" Text="Enter Course:" />
+            <asp:TextBox ID="txtCourse" runat="server"></asp:TextBox>
+
+            <asp:Button ID="btnFilter" runat="server" Text="Filter" OnClick="btnFilter_Click" />
+
+            <h3>Query Syntax Result</h3>
+            <asp:GridView ID="gvQuerySyntax" runat="server" AutoGenerateColumns="true"></asp:GridView>
+
+            <h3>Method Syntax Result</h3>
+            <asp:GridView ID="gvMethodSyntax" runat="server" AutoGenerateColumns="true"></asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebLinqProgramOne/LinqDemo.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebLinqProgramOne
+{
+    public partial class LinqDemo : System.Web.UI.Page
+
+    {
+        // Step 2: Mapping Data Model to Object Model
+        public class Student
+        {
+            public int Rno { get; set; }
+            public string Name { get; set; }
+            public string Course { get; set; }
+        }
+
+        // Sample data (LINQ to Objects)
+        List<Student> students = new List<Student>
+    {
+        new Student { Rno = 1, Name = "Amit", Course = "Java" },
+        new Student { Rno = 2, Name = "Neha", Course = "Python" },
+        new Student { Rno = 3, Name = "Ravi", Course = "Java" },
+        new Student { Rno = 4, Name = "Sara", Course = "C#" }
+    };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnFilter_Click(object sender, EventArgs e)
+        {
+            string courseName = txtCourse.Text;
+
+            // Step 3a: Query Syntax
+            var querySyntaxResult = from s in students
+                                    where s.Course.Equals(courseName, StringComparison.OrdinalIgnoreCase)
+                                    select s;
+
+            gvQuerySyntax.DataSource = querySyntaxResult.ToList();
+            gvQuerySyntax.DataBind();
+
+            // Step 3b: Method Syntax
+            var methodSyntaxResult = students
+                                     .Where(s => s.Course.Equals(courseName, StringComparison.OrdinalIgnoreCase))
+                                     .ToList();
+
+            gvMethodSyntax.DataSource = methodSyntaxResult;
+            gvMethodSyntax.DataBind();
+        }
+    }
+    }
